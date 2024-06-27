86740da433e01fee17cb833a3a585e1d834190c6
Yudain22 <di4046@gmail.com>
2024-06-27 16:16:34 +0900

    sidebar 디자인

--- Hotel_arcana/src/main/java/com/example/hotel_arcana/login/controller/IndexController.java
+++ Hotel_arcana/src/main/java/com/example/hotel_arcana/login/controller/IndexController.java
@@ -9,15 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "MyPage";
-    }
-    @GetMapping("/test1")
-    public String test1() {
-        return "/test1";
-    }
-
-    @GetMapping("/header")
-    public String header() {
-        return "/headertest";
+        return "/MyPage";
     }
 }
