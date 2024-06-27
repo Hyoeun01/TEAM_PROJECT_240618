345f008dbe93be6456020313fffba6ad1bbd45e2
Backseonghun <tjdgns3690@gmail.com>
2024-06-27 09:51:05 +0900

    드롭다운제외 + 헤더푸터 분할

--- Hotel_arcana/src/main/java/com/example/hotel_arcana/controller/IndexController.java
+++ Hotel_arcana/src/main/java/com/example/hotel_arcana/controller/IndexController.java
@@ -18,4 +18,9 @@ public class IndexController {
     public String test1() {
         return "/test1";
     }
+
+    @GetMapping("/header")
+    public String header() {
+        return "/headertest";
+    }
 }
