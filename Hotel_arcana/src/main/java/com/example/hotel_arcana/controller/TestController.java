bbff40af6150318e59e21a7c53dd71169c69a48d
Backseonghun <tjdgns3690@gmail.com>
2024-07-02 14:25:21 +0900

    9할완료

--- Hotel_arcana/src/main/java/com/example/hotel_arcana/controller/TestController.java
+++ Hotel_arcana/src/main/java/com/example/hotel_arcana/controller/TestController.java
@@ -9,4 +9,27 @@ public class TestController {
     public String test() {
         return "testfile.html";
     }
+    @GetMapping("/twin city")
+    public String room1() {
+         return "twin city view.html";
+    }
+    @GetMapping("/Presidential")
+    public String room2() {
+        return "Presidential.html";
+    }
+    @GetMapping("/Delux1")
+    public String room3() {
+        return "Delux double.html";
+    }
+    @GetMapping("/Delux2")
+    public String room4() {
+        return "Delux sweetie.html";
+    }
+    @GetMapping("/Royal")
+    public String room5() {
+        return "Royal sweet.html";
+    } @GetMapping("/Junior")
+    public String room6() {
+        return "Junior.html";
+    }
 }
